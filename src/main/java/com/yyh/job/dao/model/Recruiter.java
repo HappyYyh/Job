475206a41622248af2e46e1f16d18d42dd3703b6
昊天 <863044052@qq.com>
2019-04-05 16:08:06 +0800

    生成新的表

---
+++ src/main/java/com/yyh/job/dao/model/Recruiter.java
@@ -0,0 +1,43 @@
+package com.yyh.job.dao.model;
+
+public class Recruiter {
+    private Integer id;
+
+    private Integer userId;
+
+    private Integer companyId;
+
+    private String position;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position == null ? null : position.trim();
+    }
+}
