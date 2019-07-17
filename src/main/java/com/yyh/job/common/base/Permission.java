0bd618cbd0f18e6753ce38ad54912caa0fc3996d
haotian575 <863044052@qq.com>
2019-07-17 15:51:44 +0800

    创建注解

---
+++ src/main/java/com/yyh/job/common/base/Permission.java
@@ -0,0 +1,12 @@
+package com.yyh.job.common.base;
+
+/**
+ * All rights Reserved, Designed By yyh
+ *
+ * @Package com.yyh.job.common.base
+ * @author: yyh
+ * @date: 2019/7/17 15:46
+ * @since V1.1.0-SNAPSHOT
+ */
+public @interface Permission {
+}
